ecf0b7bfae49350f2152b334ebcf080958d0f298
Maria <mariya-ivanova@list.ru>
2024-10-26 12:42:17 +0300

    the solution was added

---
+++ task4.py
@@ -0,0 +1,5 @@
+S = int(input('введите общее количество журавликов: '))
+Petya = int(S / 6)
+Serezha = int(S / 6)
+Katya = 2 * (Petya + Serezha)
+print('Петя : ', Petya, ', Катя : ', Katya, ', Сережа : ', Serezha)
