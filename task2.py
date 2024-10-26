f412d19b262a820e0eb1ee173ee4c0fb65833766
Maria <mariya-ivanova@list.ru>
2024-10-26 12:26:08 +0300

    the solution was supplemented by checking the input number

--- task2.py
+++ task2.py
@@ -1,3 +1,6 @@
 a = int(input('give me your 3-digit number: '))
-sum = a % 10 + (a // 10 % 10) + a // 100
-print('the sum of digits: ', sum)
+if a // 100 > 0 and a // 100 <= 9:
+    sum = a % 10 + a // 10 % 10 + a // 100
+    print('the sum of digits: ', sum)
+else:
+    print('your number has more or less 3 digits. please retry')
